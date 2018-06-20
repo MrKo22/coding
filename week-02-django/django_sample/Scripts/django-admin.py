afeecca19dcaf522dd273ac8c7c4f47d9874cb3a
MrKo22 <kkw722@hotmail.com>
2018-06-20 17:04:58 -0300

    Add week-02-django project

---
+++ week-02-django/django_sample/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ko gunwoo m\dev\coding-test2\week-02-django\django_sample\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
